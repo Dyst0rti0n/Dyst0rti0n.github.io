08b6e311bfd4be20508384837098904fccf3b111
Dyst0rti0n <Dystorti0n@proton.me>
2024-07-15 03:12:26 +0100

    A few index, navbar changes

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -1,25 +1,9 @@
-document.addEventListener('DOMContentLoaded', function () {
-    var navbar = document.getElementById('navbar');
-    var lastScrollTop = 0;
-
-    window.addEventListener('scroll', function () {
-        var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-        if (scrollTop > lastScrollTop) {
-            // Downscroll
-            navbar.classList.add('hidden');
-        } else {
-            // Upscroll
-            navbar.classList.remove('hidden');
-        }
-        lastScrollTop = scrollTop <= 0 ? 0 : scrollTop; // For Mobile or negative scrolling
-    });
-
-    document.querySelector('.icon').addEventListener('click', function () {
-        var navbarCenter = document.getElementById('navbar-center');
-        if (navbarCenter.style.display === 'flex') {
-            navbarCenter.style.display = 'none';
-        } else {
-            navbarCenter.style.display = 'flex';
-        }
+document.addEventListener('DOMContentLoaded', () => {
+    const mobileMenu = document.querySelector('#mobile-menu');
+    const navbarMenu = document.querySelector('.navbar-menu');
+    
+    mobileMenu.addEventListener('click', () => {
+        mobileMenu.classList.toggle('is-active');
+        navbarMenu.classList.toggle('active');
     });
 });
