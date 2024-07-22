8618618fb18093652512a6fda7bd9b356014fd56
Dyst0rti0n <Dystorti0n@proton.me>
2024-07-23 00:12:34 +0100

    Fixing carousel

--- assets/js/githubrepos.js
+++ assets/js/githubrepos.js
@@ -22,12 +22,20 @@ function displayRepos(repos) {
     // Initialize Slick Carousel
     $(document).ready(function(){
         $('.carousel-strip').slick({
-            infinite: true,
-            slidesToShow: 3,
-            slidesToScroll: 1,
-            autoplay: true,
-            autoplaySpeed: 2000,
-            arrows: false
+          infinite: true,
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          arrows: true,
+          dots: true,
+          responsive: [
+            {
+              breakpoint: 768,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+              }
+            }
+          ]
         });
     });
 }
