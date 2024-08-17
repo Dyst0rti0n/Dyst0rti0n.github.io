3989ef15d169f5bc983a440067f0996f290eb0ec
dystortion <113265684+Dyst0rti0n@users.noreply.github.com>
2024-08-17 14:53:13 +0100

    Navbar Updates for mobile

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -1,10 +1,24 @@
 document.addEventListener('DOMContentLoaded', () => {
     const mobileMenu = document.querySelector('#mobile-menu');
     const navbarMenu = document.querySelector('.navbar-menu');
-    
+
     mobileMenu.addEventListener('click', () => {
         mobileMenu.classList.toggle('is-active');
         navbarMenu.classList.toggle('active');
+
+        if (navbarMenu.classList.contains('active')) {
+            navbarMenu.style.maxHeight = navbarMenu.scrollHeight + 'px';
+        } else {
+            navbarMenu.style.maxHeight = '0';
+        }
+    });
+
+    // Close the menu if you click outside of it
+    document.addEventListener('click', (e) => {
+        if (!navbarMenu.contains(e.target) && !mobileMenu.contains(e.target)) {
+            navbarMenu.classList.remove('active');
+            mobileMenu.classList.remove('is-active');
+            navbarMenu.style.maxHeight = '0';
+        }
     });
 });
- 
